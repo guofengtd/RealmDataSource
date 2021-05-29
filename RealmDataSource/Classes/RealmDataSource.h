56171188c73aa1fafbe5fd94966316304f018f29
xgf <xiong.guofeng@ocloudware.com>
2021-05-29 22:08:20 +0800

    first commit

---
+++ RealmDataSource/Classes/RealmDataSource.h
@@ -0,0 +1,48 @@
+//
+//  RealmDataSource.h
+//
+
+#import <Foundation/Foundation.h>
+#import <Realm/Realm.h>
+#import <Realm/RLMObjectStore.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RealmBaseObject : RLMObject
+
+@property int uid;
+@property int refCount;
+@property int loginid;
+
+@end
+
+typedef void (^RLMNotifyBlock)(RLMResults * _Nullable data);
+
+@interface RealmDataSource : NSObject
+
+@property (nonatomic, readonly) RLMRealm  *realm;
+
++ (instancetype)sharedClient;
+
+- (void)watchWithClassName:(NSString *)className
+                 predicate:(nullable NSPredicate *)predicate
+                      sort:(NSArray<RLMSortDescriptor *> *)sortDescriptors
+               notifyBlock:(RLMNotifyBlock)notifyBlock
+                       key:(NSString *)key;
+
+- (void)stopWatchForKey:(NSString *)key;
+
+- (void)addObject:(RealmBaseObject *)object;
+
+- (void)addObjects:(NSArray<RealmBaseObject *> *)array;
+
+- (void)addObjects:(nullable NSArray<RealmBaseObject *> *)array
+   objectClassName:(nullable NSString *)objectClassName
+           syncAll:(BOOL)syncAll
+         predicate:(nullable NSPredicate *)predicate;
+
+- (void)deleteObjects:(NSArray<RealmBaseObject *> *)array;
+
+@end
+
+NS_ASSUME_NONNULL_END
